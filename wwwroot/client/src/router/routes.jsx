0c26ed1681f6472c42de24a8143ea753ece0b8a1
luis <luisglobalr@gmail.com>
2025-12-08 17:03:47 -0400

    Organizadas las carpetas

---
+++ wwwroot/client/src/router/routes.jsx
@@ -0,0 +1,12 @@
+import {Routes, Router, Route} from "react-router-dom";
+
+function Rutas(){
+    <Routes>
+        <Routes/>
+        <Routes/>
+        <Routes/>
+        <Routes/>
+    </Routes>
+}
+
+export default Rutas
